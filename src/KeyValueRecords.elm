78caa753c65eb8afdc81de86a78f7520c1e7e0a0
David R. Poindexter III <drpoindexter@gmail.com>
2017-02-19 13:07:08 -0500

    Store records, and refer to with type signatures

--- src/KeyValueRecords.elm
+++ src/KeyValueRecords.elm
@@ -2,5 +2,22 @@ module Main exposing (..)
 
 import Html exposing (text)
 
+
+type alias Dog =
+    { name: String
+    , age: Int
+    }
+
+dog =
+    { name = "Spock"
+    , age = 3
+    }
+
+renderDog : Dog -> String
+renderDog dog =
+    dog.name
+        ++ " - "
+        ++ (toString dog.age)
+
 main =
-    text "Hi Records!"
+    text <| renderDog dog
