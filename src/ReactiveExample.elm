f5a8db091d6ccfb52b873504ff070c31ef78aef6
David R. Poindexter III <drpoindexter@gmail.com>
2017-02-26 13:06:10 -0500

    Add interactive html example

---
+++ src/ReactiveExample.elm
@@ -0,0 +1,37 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Json.Encode exposing (..)
+import Html exposing (beginnerProgram)
+import Html.Events exposing (..)
+-- Four Parts
+-- model, update, view
+
+model =
+    { showFace = False }
+
+type Msg =
+    ShowFace
+
+-- This is a pure function
+update msg model_ =
+    case msg of
+        ShowFace -> { model_ | showFace = True }
+
+view model =
+    div []
+        [ h1 [] [ text "Face generator" ]
+        , button [ onClick ShowFace ] [ text "Face me" ]
+        , if model.showFace then
+            span [ property "innerHTML" (Json.Encode.string "&#9786;") ] []
+          else
+            span [] []
+        ]
+
+main =
+    beginnerProgram
+        { model = model
+        , update = update
+        , view = view
+        }
